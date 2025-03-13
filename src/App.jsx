0091a67e1caa33bbfcf64cc960b7a5ca8af25311
suresh <suresh475330@gmail.com>
2025-03-13 22:01:12 +0530

    firest commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase-config";
+
+const UsersList = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  console.log(users)
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        setLoading(true);
+        const querySnapshot = await getDocs(collection(db, "Users"));
+        const usersList = querySnapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setUsers(usersList);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false)
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+      {loading && <h2>loading...</h2>}
+      <ul>
+
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UsersList;
